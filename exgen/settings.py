60b12327b9521190a70ea2a8ad48b80d3d743a44
Uzurpator <uzurpator001@gmail.com>
2019-06-30 14:38:20 +0300

    Some settings.py refacoring

--- exgen/settings.py
+++ exgen/settings.py
@@ -20,12 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-try:
-    SECRET_KEY = open('exgen/secret_key.txt', 'r',
-                      encoding='utf-8').readline().strip()
-except:
-    SECRET_KEY = open('../secret_key.txt', 'r',
-                      encoding='utf-8').readline().strip()
+SECRET_KEY = open('exgen/secret_key.txt', 'r', encoding='utf-8').readline().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
