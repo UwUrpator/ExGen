a6067d2c835b55a25161df40186a60386432d1cb
Uzurpator <uzurpator001@gmail.com>
2019-06-30 14:30:00 +0300

    Rename project

--- exgen/wsgi.py
+++ exgen/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for physgen project.
+WSGI config for exgen project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'physgen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exgen.settings')
 
 application = get_wsgi_application()
