a3f87c19cda5b7ff410f94d7cc78955c0a27825c
Uzurpator <uzurpator001@gmail.com>
2019-06-30 13:54:26 +0300

    Edited internationalization and e-mail password

--- physgen/settings.py
+++ physgen/settings.py
@@ -163,4 +163,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'exercise.gen@gmail.com'
-EMAIL_HOST_PASSWORD = 'ExGen3394746'
+EMAIL_HOST_PASSWORD = 'rnqrkmwpgjpltjpn'
