4d8a14bd3a4f8763f3126396688019189849405c
Uzurpator <uzurpator001@gmail.com>
2019-07-01 21:24:48 +0300

    Added models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=30)
+    text = models.CharField()
+
+
+class Solution(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    formula = models.CharField(max_length=255)
+
+
+class Variable(models.Model):
+    solution = models.ForeignKey(Solution, on_delete=models.CASCADE)
+    name = models.CharField(max_length=5)
+    value = models.FloatField()
