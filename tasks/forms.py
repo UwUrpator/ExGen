73ab84472548806b8f009d3c8a9be6ba0151c2fd
Uzurpator <uzurpator001@gmail.com>
2019-07-14 10:03:22 +0300

    Added tasks form

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,6 +5,10 @@ from tasks.models import Task, Variable
 
 class TaskForm(forms.ModelForm):
 
+    name = forms.CharField(max_length=30, help_text='Name of task', label='Name:')
+    text = forms.CharField(max_length=3000, help_text='Text of task', label='Text:')
+    formula = forms.CharField(max_length=256, help_text='Formula for answer', label='Formula:')
+
     class Meta:
         model = Task
         fields = ('name', 'text', 'formula')
