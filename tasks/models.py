b342174455cbc148a709f3def5705866a819ee33
Uzurpator <uzurpator001@gmail.com>
2019-07-15 14:12:43 +0300

    View to create tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+from users.models import EmailUser
+
 
 class Task(models.Model):
+    user = models.ForeignKey(EmailUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     text = models.CharField(max_length=3000)
     formula = models.CharField(max_length=255)
 
 class Variable(models.Model):
-    solution = models.ForeignKey(Task, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
     name = models.CharField(max_length=5)
     value = models.FloatField()
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 
 from tasks.forms import TaskForm, VariablesForm
+from tasks.models import Task, Variable
 
 
 def createTask(request):
     context = dict()
-    context['task'] = TaskForm
-    context['variables'] = VariablesForm
+    if request.method == 'POST':
+        task_form = TaskForm(request.POST)
+        variables_form = VariablesForm(request.POST)
+        if task_form.is_valid() and variables_form.is_valid():
+            task = Task(user=request.user, name=task_form.cleaned_data['name'],
+                        text=task_form.cleaned_data['text'], formula=task_form.cleaned_data['formula'])
+            task.save()
+
+            variables = Variable(task=task, name=variables_form.cleaned_data['name'],
+                        value=variables_form.cleaned_data['value'])
+            variables.save()
+    else:
+        task_form = TaskForm()
+        variables_form = VariablesForm()
+
+
+    context['task'] = task_form
+    context['variables'] = variables_form
     return render(request, 'tasks/create.html', context)
